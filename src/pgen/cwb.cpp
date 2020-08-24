c6d2edff2ac996f761cd1ec1799cfa8d3a499e90
Joseph Eatson [py13je] <py13je@phy-lin4281.leeds.ac.uk>
2020-08-24 02:19:35 +0100

    Working on integrating Julians featureset to athena - Modified to work with class based structure - Problem generator completely rewritten for versatility - Parts need fixing, afterwards move onto cooling code

--- src/pgen/cwb.cpp
+++ src/pgen/cwb.cpp
@@ -45,119 +45,277 @@
 
 #include "../hydro/hydro.hpp"
 #include "../coordinates/coordinates.hpp"
+#include "../scalars/scalars.hpp"
 
 
 #include "cwb.hpp"
 
 namespace {
+  // Define objects
   // Create star objects, see cwb.hpp
   Star WR;  // Object describing properties of Wolf-Rayet star
   Star OB;  // Object describing properties of OB star
+  // Vector containing pointers to both stars
+  std::vector<Star*> Stars;
 
+  // Create cooling curve class
+  CoolCurve CCurve;
+  // Star orbital parameters
+  Real dsep;
+  Real eta;
+  Real rob;
+  Real rwr;
+  Real phase_offset;
+  Real period;
+  Real ecc;
+
+  // Dust variables
   Real a_min;
   Real z_min;
   Real bulk_dens;
   Real stick_eff;
   Real nuc_temp;
 
-  bool orbit;
-  bool cool;
+  // Simulation parameters
+  int  remap = 10;     // Remap radius
+  bool orbit = false;  // Turn on orbital dynamics
+  bool cool  = false;  // Turn on cooling
 
+  // Simulation variables
+  int coord; // Coordinate system, 0 for cartesian, 1 for cylindrical
 
-  // Constants
-  Real kboltz = 1.380649e-16;
+  // Constants, all values are in CGS
+  Real massh  = 1.6735575e-21;  // Hydrogen atom mass (g)
+  Real kboltz = 1.380649e-16;   // Boltzmann constant (erg K^-1)
+  Real G      = 6.67259e-8;     // Gravitational constant (dyn cm^2 g^-2)
+  Real Msol   = 1.9891e33;      // 1 Solar mass (g)
+  Real yr     = 3.15569e7;      // Seconds in a year (s)
+  // Thermodynamic properties
+  Real g = 5.0 / 3.0;  // Ratio of specific heats, gamma
+  Real g1 = g - 1;     // Gamma-1
 }
 
-bool IsInRemap(Real x, Real y, Real z, Star Star) {
-  Real x0 = Star.x1;
-  Real y0 = Star.x2;
-  Real z0 = Star.x3;
-  Real rad = std::sqrt(SQR(x - x0) + SQR(y - y0) + SQR(z - z0));
-  if (rad <= Star.remap_cgs) {return true;}
-  return false;
+void CalcOrbit(Real t) {
+  Real time_offset;  // Time offset (s)
+  Real t_orbit;      // Orbital time, inclusive of offset (s)
+  Real phase;        // Orbital phase, fraction
+
+  // Keplerian orbital parameters
+  Real phi;          // True anomaly (rad)
+  Real E;            // Eccentric anomaly
+  Real sinE, cosE;   // Sine and cosine components of E
+  Real dE;           // Change in eccentric anomaly
+
+  Real sii,coi;
+  Real a1,a2;
+  Real v1,v2;
+  Real theta,ang;
+  Real rrel;
+  Real M,gamma_ang;
+  Real dx,dy; 
+
+
+  time_offset = phase_offset * period;
+  t_orbit     = t + time_offset;
+  phase       = t_orbit / period;
+
+  phi  = 2.0 * PI * phase; // Calculate true anomaly
+  E    = phi;              // First guess at eccentric anomaly
+  sinE = std::sin(E);      // Calculate sine component
+  cosE = std::cos(E);      // Calculate cosine component
+  dE   = (phi - E + ecc * sinE)/(1.0 - ecc*cosE); 
+
+  while (std::abs(dE) > 1.0e-10) {
+    E = E + dE;
+    sinE = std::sin(E);
+    cosE = std::cos(E);
+    dE = (phi - E + ecc * sinE) / (1.0 - ecc * cosE);
+  }
+
+  sii = (std::sqrt(1.0 - SQR(ecc))) * sinE / (1.0 - ecc*cosE);
+  coi = (cosE - ecc) / (1.0 - ecc*cosE);
+  theta = std::atan2(sii,coi);
+  // Ensure that theta is between 0 and 2*PI
+  if (theta < 0.0) {theta = 2.0 * PI + theta;}
+  // Calc radius vector
+  rrel = 1.0 - ecc*cosE;
+
+  // Compute barycentric masses
+  M  = (std::pow(OB.mass,3)/std::pow((WR.mass+OB.mass),2)) * Msol;
+  a1 = std::pow((G*M*period*period/(4.0*PI*PI)),(1.0/3.0));
+  v1 = std::sqrt(G*M*(2.0/rrel/a1 - 1.0/a1));
+  M  = (std::pow(WR.mass,3)/std::pow((WR.mass+OB.mass),2)) * Msol;
+  a2 = std::pow((G*M*period*period/(4.0*PI*PI)),(1.0/3.0));
+  v2 = std::sqrt(G*M*(2.0/rrel/a2 - 1.0/a2));
+
+  // Compute angle between velocity and radius vectors
+
+  gamma_ang = PI/2.0 + std::acos(std::sqrt((1.0 - ecc*ecc)/(rrel*(2.0 - rrel))));
+
+  if (theta <= PI) {ang = PI - gamma_ang + theta;}
+  else             {ang = theta + gamma_ang;}
+
+  // Orbit is in the xy plane, with the semi-major axis along the y-axis
+  // Hence, z is not updated, and is zeroed on init
+  WR.x[0] =  a1 * rrel * std::sin(theta);
+  WR.x[1] = -a1 * rrel * std::cos(theta);
+  OB.x[0] = -a2 * rrel * std::sin(theta);
+  OB.x[1] =  a2 * rrel * std::cos(theta);
+  WR.v[0] =  v1 * std::sin(ang);
+  WR.v[1] = -v1 * std::cos(ang);
+  OB.v[0] = -v2 * std::sin(ang);
+  OB.v[1] =  v2 * std::cos(ang);
+  // Update dsep and rob
+  dx    = WR.x[0] - OB.x[0];
+  dy    = WR.x[1] - OB.x[1];
+  dsep  = std::sqrt(SQR(dx) + SQR(dy));
+  rob   = (std::sqrt(eta)/(1.0 + std::sqrt(eta))) * dsep;
+  rwr   = dsep - rob;
 }
 
-// Map winds from both stars, needs some cleanup
+void RemapWinds(MeshBlock *pmb, const Real time, const Real dt,
+                const AthenaArray<Real> &prim,
+                const AthenaArray<Real> &bcc,
+                AthenaArray<Real> &cons) {
 
-void RemapWind(MeshBlock *pmb, const Real time, const Real dt,
-               const AthenaArray<Real> &prim,
-               const AthenaArray<Real> &bcc,
-               AthenaArray<Real> &cons) {
+  // First, calculate the remap radius in CGS units, rather than cell widths
+  Real remap_radius = Real(remap) * pmb->pcoord->dx1v(0);
   
-  int is = pmb->is, ie = pmb->ie;
-  int js = pmb->js, je = pmb->je;
-  int ks = pmb->ks, ke = pmb->ke;
+  // As a meshblock is refined down to the appropriate level it
+  // needs to be in an adaptive mesh, this means that the mesh
+  // itself is ordered, therefore rather than looping over
+  // the entire mesh, only a cubic region around the remap zone
+  // is looped through.
+  // i/j/kstar: Closest index on axis to star position
+  // i/j/kstl:  "Lower" array to access
+  // i/j/kstu:  "Upper" array to access
 
-  for (int k=ks; k<=ke; k++) {
-    for (int j=js; j<=je; j++) {
-      for (int i=is; i<=ie; i++) {
-        Real x   = pmb->pcoord->x1v(i);
-        Real y   = pmb->pcoord->x2v(j);
-        Real z   = pmb->pcoord->x3v(k);
-        if (IsInRemap(x,y,z,WR) == true) {
-          Real rhodot   = WR.mdot_cgs / WR.remap_vol;
-          Real mech_lum = 0.5 * rhodot * SQR(WR.vinf);
-          cons(IDN,k,j,i) += rhodot * dt;
-          cons(IEN,k,j,i) += mech_lum * dt;
-        }
-        else if (IsInRemap(x,y,z,OB) == true) {
-          Real rhodot   = OB.mdot_cgs / OB.remap_vol;
-          Real mech_lum = 0.5 * rhodot * SQR(OB.vinf);
-          cons(IDN,k,j,i) += rhodot * dt;
-          cons(IEN,k,j,i) += mech_lum * dt;
+  for (auto Star : Stars) {
+    int istar = int((Star->x[0] - pmb->pcoord->x1f(0))/pmb->pcoord->dx1f(0));
+    int jstar = int((Star->x[1] - pmb->pcoord->x2f(0))/pmb->pcoord->dx2f(0));
+    int kstar = int((Star->x[2] - pmb->pcoord->x3f(0))/pmb->pcoord->dx3f(0));
+    int istl = std::max(pmb->is,istar-remap-2);
+    int jstl = std::max(pmb->js,jstar-remap-2);
+    int kstl = std::max(pmb->ks,kstar-remap-2);
+    int istu = std::min(pmb->ie,istar+remap+2);
+    int jstu = std::min(pmb->je,jstar+remap+2);
+    int kstu = std::min(pmb->ke,kstar+remap+2);
+    // In the case of a cylindrical coordinate system, phi axis modified
+    if (coord == CYL) {
+      jstl = pmb->js;
+      jstu = pmb->je;
+    }
+    // Loop through cubic region around star
+    for (int k=kstl; k<=kstu; ++k) {
+      Real zc  = pmb->pcoord->x3v(k) - Star->x[2];
+      Real zc2 = SQR(zc);
+      for (int j=jstl; j<=jstu; ++j) {
+        Real yc  = pmb->pcoord->x2v(k) - Star->x[1];
+        Real yc2 = SQR(yc);
+        for (int i=istl; i<=istu; ++i) {
+          Real xc  = pmb->pcoord->x1v(k) - Star->x[0];
+          Real xc2 = SQR(xc);
+          Real r2  = xc2 + yc2 + zc2;
+          Real r   = std::sqrt(r2);
+          // Check to see if cell is within remap zone
+          if (r < remap_radius) {
+            Real xy = xy = std::sqrt(xc2 + yc2);
+            Real sinphi = xy/r;
+            Real cosphi = zc/r;
+            
+            Real rho = Star->mdot_cgs / (4.0 * PI * r2 * Star->vinf);
+            Real u1, u2, u3;
+            Real pre = (rho / Star->avgm) * kboltz * Star->twnd;
+            Real ke  = 0.5 * rho * SQR(Star->vinf);
+
+            if (coord == CART) {
+              Real costht = xc/xy;
+              Real sintht = yc/xy;
+              u1 = Star->vinf * sinphi * costht;
+              u2 = Star->vinf * sinphi * sintht;
+              u3 = Star->vinf * cosphi;
+            }
+            else if (coord = CYL) {
+              u1 = Star->vinf * sinphi;
+              u2 = 0.0;
+              u3 = Star->vinf * cosphi;
+            }
+            // Rewrite conserved variables in cell
+            cons(IDN,k,j,i) = rho;
+            cons(IM1,k,j,i) = rho * u1;
+            cons(IM2,k,j,i) = rho * u2;
+            cons(IM3,k,j,i) = rho * u3;
+            cons(IEN,k,j,i) = pre/g1 + ke;
+            // Use 1st scalar as marker for each star
+            if (NSCALARS > 0) {
+              pmb->pscalars->s(0,k,j,i) = Star->scal * rho;
+              pmb->pscalars->r(0,k,j,i) = Star->scal;
+            }
+          }
         }
       }
     }
   }
+
   return;
 }
-
-// Not used right now, but a basic cooling function from another code, needs to be revised to fit plasma cooling curves
 
 void CoolingFunction(MeshBlock *pmb, const Real time, const Real dt,
                      const AthenaArray<Real> &prim,
                      const AthenaArray<Real> &bcc, AthenaArray<Real> &cons)
 {
-  Real g = 1.666667;
-  Real tau = 0.01;
-  for (int k=pmb->ks; k<=pmb->ke; ++k) {
-    for (int j=pmb->js; j<=pmb->je; ++j) {
-      for (int i=pmb->is; i<=pmb->ie; ++i) {
-        Real temp = (g-1.0)*prim(IEN,k,j,i)/prim(IDN,k,j,i);
-        cons(IEN,k,j,i) -= dt*prim(IDN,k,j,i)*(temp - 10.0)/tau/(g-1.0);
+  for (auto Star : Stars) {
+    for (int k=pmb->ks; k<=pmb->ke; ++k) {
+      for (int j=pmb->js; j<=pmb->je; ++j) {
+        for (int i=pmb->is; i<=pmb->ie; ++i) {
+          Real T   = (prim(IEN,k,j,i) * Star->avgm)/(prim(IDN,k,j,i) * kboltz);
+          Real rho = prim(IDN,k,j,i);
+          Real wnd = pmb->pscalars->r(0,k,j,i);
+          // Calculate temperature loss due to plasma emission
+          Real Lambda = CCurve.FindLambda(T);
+          Real eConst = SQR(rho) / SQR(massh);
+          Real eLoss  = eConst * Lambda * dt;
+          // Remove energy from convserved energy
+          cons(IEN,k,j,i) -= eLoss * wnd;
+        }
       }
     }
   }
-  return;
 }
 
-int RefineShocks(MeshBlock *pmb) {
-  AthenaArray<Real> &w = pmb->phydro->w;
-  Real maxeps=0.0;
-  for (int k=pmb->ks; k<=pmb->ke ; k++) {
-    for(int j=pmb->js; j<=pmb->je; j++) {
-      for(int i=pmb->is; i<=pmb->ie; i++) {
-        Real epsr= (std::abs(w(IDN,k,j,i+1)-2.0*w(IDN,k,j,i)+w(IDN,k,j,i-1))
-                  +std::abs(w(IDN,k,j+1,i)-2.0*w(IDN,k,j,i)+w(IDN,k,j-1,i)))/w(IDN,k,j,i);
-        Real epsp= (std::abs(w(IEN,k,j,i+1)-2.0*w(IEN,k,j,i)+w(IEN,k,j,i-1))
-                  +std::abs(w(IEN,k,j+1,i)-2.0*w(IEN,k,j,i)+w(IEN,k,j-1,i)))/w(IEN,k,j,i);
-        Real eps = std::max(epsr, epsp);
-        maxeps = std::max(maxeps, eps);
+int CWBRefinementCondition(MeshBlock *pmb) {
+  Real dx = pmb->pcoord->dx1f(0);
+  Real dy = pmb->pcoord->dx2f(0);
+  Real dz = pmb->pcoord->dx3f(0);
+
+  // Check to see if cell is within 10(?) coarse cells of remap zone
+  bool derefineWind = true;
+  for (auto Star : Stars) {
+    for (int k=pmb->ks; k<=pmb->ke; ++k) {
+      Real zc = pmb->pcoord->x3v(k) - Star->x[2];
+      for (int j=pmb->js; j<=pmb->je; ++j) {
+        Real yc = pmb->pcoord->x2v(j) - Star->x[1];
+        for (int i=pmb->is; i<=pmb->ie; ++i) {
+          Real xc  = pmb->pcoord->x1v(i) - Star->x[0];
+          Real rad = std::sqrt(SQR(xc) + SQR(yc) + SQR(zc));
+          int ri = int(rad / dx);
+          if      (ri < 10) {return 1;}
+          else if (ri < 20) {derefineWind = false;}
+        }
       }
     }
   }
-  if(maxeps > 0.01) return 1;
-  if(maxeps < 0.005) return -1;
+  if (derefineWind) {return -1;}
+  // All checks passed, no change
   return 0;
 }
+
+
+
 
 // int RefineRemaps(MeshBlock *pmb) {
 //   return ;
 // } 
-
-
-// 3x members of Mesh class:
-
 //========================================================================================
 //! \fn void Mesh::InitUserMeshData(ParameterInput *pin)
 //  \brief Function to initialize problem-specific data in Mesh class.  Can also be used
@@ -166,17 +324,30 @@ int RefineShocks(MeshBlock *pmb) {
 //========================================================================================
 
 void Mesh::InitUserMeshData(ParameterInput *pin) {
+  // Get boolean values from problem file
+  orbit = pin->GetBoolean("problem","orbit");
+  cool  = pin->GetBoolean("problem","cool");
+
   // Initialise stars
-  WR.Init(pin,"wr");
-  OB.Init(pin,"ob");
+  WR.Init(pin,"wr",1);
+  OB.Init(pin,"ob",0);
+  // Initialise star vector
+  Stars.push_back(&WR);
+  Stars.push_back(&OB);
+  // Initialise cooling curve
+  if (cool) {
+    std::string coolcurvefilename = pin->GetString("problem","coolcurve");
+    CCurve.Init(coolcurvefilename);
+  }
 
   // Initialise dust
   a_min = pin->GetReal("problem","a_min");
   z_min = pin->GetReal("problem","z_min");
 
-  // Get boolean values from problem file
-  orbit = pin->GetBoolean("problem","orbit");
-  cool  = pin->GetBoolean("problem","cool");
+  // Get orbit parameters from problem file
+  period       = pin->GetReal("problem","period");
+  ecc          = pin->GetReal("problem","ecc");
+  phase_offset = pin->GetReal("problem","phase_offset");
 
   // Get dust parameters
   bulk_dens = pin->GetReal("problem","bulk_dens");
@@ -186,12 +357,11 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
   // Enroll functions based on boolean values
   // Refinement functions
   if (adaptive) {
-    EnrollUserRefinementCondition(RefineShocks);
+    EnrollUserRefinementCondition(CWBRefinementCondition);
   }
   // Source functions
-  EnrollUserExplicitSourceFunction(RemapWind);
-  // if (cool) {EnrollUserExplicitSourceFunction(CoolingFunction);}
-
+  EnrollUserExplicitSourceFunction(RemapWinds);
+  
   return;
 }
 
@@ -234,46 +404,106 @@ void MeshBlock::InitUserMeshBlockData(ParameterInput *pin) {
 //! \fn void MeshBlock::ProblemGenerator(ParameterInput *pin)
 //  \brief CWB problem generator
 //========================================================================================
+
+// void MeshBlock::ProblemGenerator(ParameterInput *pin) {
+
+// }
  
 
 void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   Real d_amb = pin->GetReal("problem","d_amb");
   Real p_amb = pin->GetReal("problem","p_amb");
 
-  for (int k=ks; k<=ke; ++k) {
-    for (int j=js; j<=je; ++j) {
-      for (int i=is; i<=ie; ++i) {
+  eta = (OB.mdot*OB.vinf)/(WR.mdot*WR.vinf);
 
-        Real x0 = WR.x1;
-        Real y0 = WR.x2;
-        Real z0 = WR.x3;
-        Real rad;
+  // If orbits are enabled, recalculate positions and parameters
+  if (orbit) {
+    for (auto Star : Stars) {Star->x[2] = 0.0;}
+    CalcOrbit(0.0); // Find orbits at t = 0
+  }
 
-        if (std::strcmp(COORDINATE_SYSTEM, "cartesian") == 0) {
-          Real x = pcoord->x1v(i);
-          Real y = pcoord->x2v(j);
-          Real z = pcoord->x3v(k);
-          rad = std::sqrt(SQR(x - x0) + SQR(y - y0) + SQR(z - z0));
-        } else if (std::strcmp(COORDINATE_SYSTEM, "cylindrical") == 0) {
-          Real x = pcoord->x1v(i)*std::cos(pcoord->x2v(j));
-          Real y = pcoord->x1v(i)*std::sin(pcoord->x2v(j));
-          Real z = pcoord->x3v(k);
-          rad = std::sqrt(SQR(x - x0) + SQR(y - y0) + SQR(z - z0));
-        } else { // if (std::strcmp(COORDINATE_SYSTEM, "spherical_polar") == 0)
-          Real x = pcoord->x1v(i)*std::sin(pcoord->x2v(j))*std::cos(pcoord->x3v(k));
-          Real y = pcoord->x1v(i)*std::sin(pcoord->x2v(j))*std::sin(pcoord->x3v(k));
-          Real z = pcoord->x1v(i)*std::cos(pcoord->x2v(j));
-          rad = std::sqrt(SQR(x - x0) + SQR(y - y0) + SQR(z - z0));
-        }
 
-        Real rho_sw = WR.mdot_cgs / (4.0*PI*rad*rad*WR.vinf);
-        Real pg_sw  = (rho_sw * kboltz * WR.twnd) / (WR.avgm);
+  
 
-        phydro->u(IDN,k,j,i) = rho_sw * 0.0001;
-        phydro->u(IEN,k,j,i) = pg_sw * 0.0001;
+
+  // Check to see if in a valid co-ordinate system, otherwise, exit with errors
+  // Coordinate described with integer value from here on out
+  // This is done as strcmp() is quite costly, especially within loops
+  if (std::strcmp(COORDINATE_SYSTEM, "cartesian") == 0) {coord = CART;}
+  else if (std::strcmp(COORDINATE_SYSTEM, "cylindrical") == 0) {coord = CYL;}
+  else {
+    std::stringstream msg;
+    msg << "### FATAL ERROR in cwb.cpp ProblemGenerator" << std::endl
+        << "Unrecognized COORDINATE_SYSTEM=" << COORDINATE_SYSTEM << std::endl;
+    ATHENA_ERROR(msg);
+  }
+  // This makes the assumption that WR is the primary star!!!
+  Real dx = WR.x[0] - OB.x[0];
+  Real dy = WR.x[1] - OB.x[1];
+  Real theta = std::atan2(dy,dx);
+  Real stagx = OB.x[0] + (rob * std::cos(theta));
+  Real stagy = OB.x[1] + (rob * std::sin(theta));
+  // Run through grid in meshblock, finding the side the cell is on,
+  // then rewriting properties based on single wind outflow from star
+  // the cell is associated with
+  for (int k=ks; k<=ke; k++) {
+    Real z  = pcoord->x3v(k);
+    Real z2 = SQR(z);
+    for (int j=js; j<=je; j++) {
+      Real y  = pcoord->x2v(j);
+      Real y2 = SQR(y); 
+      for (int i=is; i<=ie; i++) {
+        Real x  = pcoord->x1v(i);
+        Real x2 = SQR(x);
+        // Translate coordinates to centre around stagnation point
+        Real xp = x - stagx;
+        Real yp = y - stagy;
+        // Rotate coordinate system such that stars run along 
+        Real xpp = (xp * std::cos(theta)) + (yp * std::sin(theta));
+        Real ypp = (yp * std::cos(theta)) - (xp * std::sin(theta));
+        // In rotated coordinate system, determine which side of the
+        // stagnation point the cell is on
+        int sid;
+        if      (xpp >= 0) {sid = 0;} // Wind is on WR side
+        else if (xpp <= 0) {sid = 1;} // Wind is on OB side
+        // Find the distance relative to the star in question
+        Real xc  = x - Stars[sid]->x[0];
+        Real yc  = y - Stars[sid]->x[1];
+        Real zc  = z - Stars[sid]->x[2];
+        Real xc2 = SQR(xc);
+        Real yc2 = SQR(yc);
+        Real zc2 = SQR(zc);
+        Real r2  = xc2 + yc2 + zc2;       // Square of distance from star
+        Real r   = std::sqrt(r2);         // Distance from star
+        Real xy  = std::sqrt(xc2 + yc2);  // xy axis distance from star
+        // Calculate angles, names should be self-explanitory
+        Real sinphi = xy/r;
+        Real cosphi = zc/r;
+        Real costht = xc/xy;
+        Real sintht = yc/xy;
+        // Copy star properties from class
+        Real mdot = Stars[sid]->mdot_cgs;  // Mass loss rate (g s^-1)
+        Real vinf = Stars[sid]->vinf;      // Terminal velocity (cm s^-1)
+        Real twnd = Stars[sid]->twnd;      // Wind temperature (K)
+        // Calculate cell properties based on distance from star
+        // Density, pressure, kinetic energy and velocities in all dimensions
+        Real rho = Stars[sid]->mdot_cgs / (4.0 * PI * r2 * WR.vinf);
+        Real pre = (rho/WR.avgm) * kboltz * WR.twnd;
+        Real ke  = 0.5 * rho * SQR(vinf);
+        Real u1  = vinf * sinphi * costht;
+        Real u2  = vinf * sinphi * sintht;
+        Real u3  = vinf * cosphi;
+        // Modify properties of cell
+        phydro->u(IDN,k,j,i) = rho;
+        phydro->u(IM1,k,j,i) = rho * u1;
+        phydro->u(IM2,k,j,i) = rho * u2;
+        phydro->u(IM3,k,j,i) = rho * u3;
+        phydro->u(IEN,k,j,i) = (pre/g1) + ke;
       }
     }
   }
+
+  return;
 }
 
 //========================================================================================
@@ -282,7 +512,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 //========================================================================================
 
 void MeshBlock::UserWorkInLoop() {
-  // do nothing
+  if (orbit) {CalcOrbit(pmy_mesh->time);}
   return;
 }
 
--- src/pgen/cwb.hpp
+++ src/pgen/cwb.hpp
@@ -8,10 +8,14 @@
 #include <sstream>
 #include <stdexcept>
 #include <string>
+#include <fstream>
 
 // Athena++ headers
 #include "../athena.hpp"
 #include "../athena_arrays.hpp"
+
+#define CART 0
+#define CYL  1
 
 //! \class Star
 //  \brief Class used to describe a star in a CWB problem
@@ -19,8 +23,9 @@
 class Star {
   public:
     // Imported variables
-    Real x1,x2,x3;   // Position (cm)
-    Real v1,v2,v3;   // Velocity (cm s^-1)
+    Real x[3];   // Position (cm)
+    Real v[3];   // Velocity (cm s^-1)
+    Real mass;
     Real mdot;       // Mass loss rate (msol yr^-1)
     Real vinf;       // Terminal velocity (cm s^-1)
     Real twnd;       // Wind temperature (K)
@@ -32,16 +37,20 @@ class Star {
     Real remap_vol;  // Remap volume (cm^3)
     Real mu;         // Mean molecular mass
     Real avgm;       // Mean molecular mass (g)
+    // Scalars
+    int  scal;       // Identifying scalar, 1 for WR wind, 0 for OB
     // Functions
-    int Init(ParameterInput *pin , std::string id);
+    int Init(ParameterInput *pin , std::string id, int scal_id);
 };
 
-int Star::Init(ParameterInput *pin, std::string id){
+int Star::Init(ParameterInput *pin, std::string id, int scal_id){
   // Read from model file:
-  // Get star position
-  x1 = pin->GetReal("problem","x1_"+id);
-  x2 = pin->GetReal("problem","x2_"+id);
-  x3 = pin->GetReal("problem","x3_"+id);
+  // Get star properties
+  x[0] = pin->GetReal("problem","x1_"+id);
+  x[1] = pin->GetReal("problem","x2_"+id);
+  x[2] = pin->GetReal("problem","x3_"+id);
+
+  mass = pin->GetReal("problem","mass_"+id);
   // Get wind properties
   mdot = pin->GetReal("problem","mdot_"+id);
   vinf = pin->GetReal("problem","vinf_"+id);
@@ -54,9 +63,9 @@ int Star::Init(ParameterInput *pin, std::string id){
   remap = pin->GetReal("problem","remap_"+id);
 
   // Initialise star velocity
-  v1 = 0.0;
-  v2 = 0.0;
-  v3 = 0.0;
+  v[0] = 0.0;
+  v[1] = 0.0;
+  v[2] = 0.0;
 
   // Calculate and/or convert additional variables
 
@@ -78,4 +87,102 @@ int Star::Init(ParameterInput *pin, std::string id){
   Real massh = 1.67355e-24;
   mu         = 1.0 / ((2 * X) + (0.75 * Y) + (0.5 * Z));
   avgm       = mu*massh;
+  // Set scalar value, which is used to identify wind
+  scal = scal_id;
+}
+
+//! \class CoolCurve
+//  \brief 
+
+class CoolCurve {
+  public:
+    // Variables
+    std::vector<Real> temp;   // Temperature (K)
+    std::vector<Real> lambda;  // Energy loss constant
+    Real tmin, tmax;  // Values of first and last index in temp
+    Real gmin, gmax;  // Values of first and last index in lambda
+    // Functions
+    int  Init(std::string coolCurveFileName);
+    Real FindLambda(Real t);
+};
+
+//! \fn Init
+//  \brief Initialise cooling curve by reading in a space separated file
+//  each line containing a pair of values, temperature and lambda.
+//  Each line is then read into the appropriate vector.
+//  Function checks for validity of file, as well as simple check to determine
+//  if vectors are the same length.
+
+int CoolCurve::Init(std::string coolCurveFileName) {
+  std::ifstream file(coolCurveFileName);
+  if (!file) {
+    std::stringstream msg;
+    msg << "### FATAL ERROR in cwb.hpp CoolCurve::Init" <<
+           std::endl <<
+           "File " <<
+           coolCurveFileName <<
+           " Cannot be read!" <<
+           std::endl;
+    ATHENA_ERROR(msg);
+    return 1;
+  }
+  else {
+    Real tbuf,lbuf;
+    std::string line;
+    file.seekg(0);
+    while (!file.eof()) {
+      file >> tbuf;
+      file >> lbuf;
+      if (lbuf == tbuf) {break;}
+      temp.push_back(tbuf);
+      lambda.push_back(lbuf);
+    }
+  }
+
+  // Convert log(T) to T
+  for (int n = 0; n < temp.size(); n++) {
+    temp[n] = pow(10.0,temp[n]);
+  }
+
+  // Compare lengths of vectors, if they are not the same, import has failed
+  if (temp.size() != lambda.size()) {
+    std::stringstream msg;
+    msg << "### FATAL ERROR in cwb.hpp CoolCurve::Init" <<
+           std::endl <<
+           "Import has failed, mismatch in temperature and lambda vector size" <<
+           std::endl;
+    ATHENA_ERROR(msg);
+  }
+
+  tmin = temp.front();
+  tmax = temp.back();
+  gmin = lambda.front();
+  gmax = lambda.back();
+
+  return 0;
+}
+
+//! \fn FindLambda
+// Perform a binary search to find first temperature in cooling curve > input temperature, tu
+// Function then performs a linear interpolation between tu and the previous temperature in index, tl, to find the interpolated value for lambda
+// Fast, but not particularly accurate at lower temperatures
+// Uses C++ algorithm library
+Real CoolCurve::FindLambda(Real t) {
+  // Quickly return values that would cause search to fail
+  if      (t <= tmin) {return 0.0;}
+  else if (t >= tmax) {return gmax;}
+  // Perform binary search
+  auto upper = std::upper_bound(temp.begin(), temp.end(), t);
+  // Assign indexes based on search
+  int  iu    = std::distance(temp.begin(), upper);
+  int  il    = iu - 1;
+  // Perform linear interpolation
+  // Assign values from each array from indexes found by binary search
+  Real tl = temp[il];
+  Real tu = temp[iu];
+  Real gl = lambda[il];
+  Real gu = lambda[iu];
+  // Calculate interpolated value of lambda
+  Real g  = gl + ((t - tl) * ((gu - gl) / (tu - tl)));
+  return g;
 }
